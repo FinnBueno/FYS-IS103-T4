b1e6ff248ada4e168567b94ab92eea2e63f7a022
Tim van Ekert <t.v.ekert@gmail.com>
2017-11-21 13:30:13 +0100

    Home pagina toegevoegd

---
+++ src/main/java/me/is103t4/corendonluggagesystem/scenes/main/tabs/HomeController.java
@@ -0,0 +1,23 @@
+package me.is103t4.corendonluggagesystem.scenes.main.tabs;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import me.is103t4.corendonluggagesystem.scenes.Controller;
+
+/**
+ * Home pagina controller aanmaken
+ *
+ * @author Tim van Ekert
+ */
+public class HomeController extends Controller {
+
+   
+    
+}
