5379f4909efba35caef746fde7aaff0eb3c37974
Sebastiaan\Sebastiaan Wezenberg <sebastiaanwezenberg@live.nl>
2017-12-19 12:16:13 +0100

    Updated configurations back end, and added preferencesManager

--- src/main/java/me/is103t4/corendonluggagesystem/scenes/main/tabs/ConfigurationsController.java
+++ src/main/java/me/is103t4/corendonluggagesystem/scenes/main/tabs/ConfigurationsController.java
@@ -7,6 +7,7 @@ package me.is103t4.corendonluggagesystem.scenes.main.tabs;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +16,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import me.is103t4.corendonluggagesystem.scenes.Controller;
 import me.is103t4.corendonluggagesystem.scenes.main.Tabs;
+import me.is103t4.corendonluggagesystem.util.PreferencesManager;
 
 /**
  *
@@ -35,34 +37,38 @@ public class ConfigurationsController extends Controller {
     private TextField emailField;
     
     @FXML
-    private TextField dateOfBirthField;
-    
-    @FXML
-    private Button saveButton;
-    
-    @FXML
-    private Label label;
+    private TextField sendgridKeyField;
 
     @Override 
     public boolean isOpen() {
         return Tabs.CONFIGURATIONS.isOpen(0);
     } 
     
+    @Override
+    public void postInit() {
+        // turn some textfields into numberfields
+        phoneNumberField.textProperty().
+                addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+                    if (!newValue.matches("\\d*")) {
+                        phoneNumberField.setText(newValue.
+                                replaceAll("[^\\d]", ""));
+                    }
+                });
+        sendgridKeyField.setText(PreferencesManager.get().get(PreferencesManager.SENDGRIDKEY));
+    }
     
     @FXML
-    private void saveChanges() {
+    public void saveChanges() {
         if (checkEmptyFields()){
             return;
-        }
-        
-        
-        
+        }      
+        PreferencesManager.get().set(PreferencesManager.SENDGRIDKEY, sendgridKeyField.getText());         
     }
     
     /**
-     * check of de invoervelden niet zijn ingevuld
+     * check if input fields are not empty
      * 
-     * @return true wanneer field leeg is
+     * @return true when fields are empty
      */
     private boolean checkEmptyFields() {
  
@@ -75,17 +81,17 @@ public class ConfigurationsController extends Controller {
             return true;
         }
         if (phoneNumberField.getText() == null || phoneNumberField.getText().length() == 0) {
-            alert("Phone number cannot be empty!");
+            alert("Phone number cannot be empty!");                                          
             return true;
         }
         if (emailField.getText() == null || emailField.getText().length() == 0) {
             alert("Email cannot be empty!");
             return true;
+        }     
+        if (sendgridKeyField.getText() == null || emailField.getText().length() == 0) {
+            alert("sendgrid key cannot be empty!");
+            return true;
         }
-//        if (dateOfBirthField.getText() == null || dateOfBirthField.getText().length() == 0) {
-//            alert("Date of birth cannot be empty!");
-//            return true;
-//        }
         return false;
     }
 }
