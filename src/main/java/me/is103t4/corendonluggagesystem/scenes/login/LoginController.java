d1b7f9380459512ce6e485ffd1766cde14296792
Sebastiaan\Sebastiaan Wezenberg <sebastiaanwezenberg@live.nl>
2018-01-14 17:18:28 +0100

    fixed bug: postInit called on EVERY login (#46)

--- src/main/java/me/is103t4/corendonluggagesystem/scenes/login/LoginController.java
+++ src/main/java/me/is103t4/corendonluggagesystem/scenes/login/LoginController.java
@@ -25,7 +25,8 @@ import me.is103t4.corendonluggagesystem.scenes.main.MainFrameController;
  * @author Finn Bon
  */
 public class LoginController extends Controller {
-
+    private boolean loggedInOnce = false;
+    
     @FXML
     private TextField usernameField;
 
@@ -55,61 +56,64 @@ public class LoginController extends Controller {
 
     @FXML
     private void login() {
-        String usernameInput = usernameField.getText();
-        if (usernameInput.length() == 0 || passwordField.getText().
-                length() == 0) {
-            errorLabel.setText("You must enter a username and password!");
-            return;
-        }
-
-        if (!usernameInput.contains("#") || usernameInput.length() != 9) {
-            errorLabel.
-                    setText("A username must be formatted like 'tag#4-digit-code'");
-            return;
-        }
-
-        String[] split = usernameInput.split("#");
-        if (split[0].length() != 4 || split[0].matches("\\d+") || split[1].length() != 4 || !split[1].matches("\\d+")) {
-            errorLabel.setText("A username must be formatted like 'tag#4-digit-code'");
-            return;
-        }
-        String name = split[0];
-        String code = split[1];
-
-        LoginTask task = new LoginTask(code, name, passwordField.getText());
-        loginButton.setDisable(true);
-        task.setOnCancelled(event -> {
-            displayErrorAlert();
-        });
-        task.setOnFailed(event -> {
-            displayErrorAlert();
-        });
-        task.setOnSucceeded((Event event) -> {
-
-            System.out.println("Test2");
-            Account account = (Account) task.getValue();
-            Alert alert;
-            if (account == null) {
-                errorLabel.setText("Invalid login!");
-                alert = new Alert(AlertType.WARNING);
-                alert.setTitle("Dialog");
-                alert.setHeaderText(null);
-                alert.setContentText("Invalid credentials!");
-                alert.showAndWait();
-            } else {
-                System.out.println("Test3");
-                account.login();
-                errorLabel.setText("");
-                MainFrameController mc = (MainFrameController) Scenes.MAIN.
-                        getController();
-                mc.fillTabPane();
-                Scenes.MAIN.setToScene();
+        if (loggedInOnce == false) {
+            loggedInOnce = true;
+            String usernameInput = usernameField.getText();
+            if (usernameInput.length() == 0 || passwordField.getText().
+                    length() == 0) {
+                errorLabel.setText("You must enter a username and password!");
+                return;
             }
-            usernameField.setText("");
-            passwordField.setText("");
 
-            loginButton.setDisable(false);
-        });
+            if (!usernameInput.contains("#") || usernameInput.length() != 9) {
+                errorLabel.
+                        setText("A username must be formatted like 'tag#4-digit-code'");
+                return;
+            }
+
+            String[] split = usernameInput.split("#");
+            if (split[0].length() != 4 || split[0].matches("\\d+") || split[1].length() != 4 || !split[1].matches("\\d+")) {
+                errorLabel.setText("A username must be formatted like 'tag#4-digit-code'");
+                return;
+            }
+            String name = split[0];
+            String code = split[1];
+
+            LoginTask task = new LoginTask(code, name, passwordField.getText());
+            loginButton.setDisable(true);
+            task.setOnCancelled(event -> {
+                displayErrorAlert();
+            });
+            task.setOnFailed(event -> {
+                displayErrorAlert();
+            });
+            task.setOnSucceeded((Event event) -> {
+
+                System.out.println("Test2");
+                Account account = (Account) task.getValue();
+                Alert alert;
+                if (account == null) {
+                    errorLabel.setText("Invalid login!");
+                    alert = new Alert(AlertType.WARNING);
+                    alert.setTitle("Dialog");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Invalid credentials!");
+                    alert.showAndWait();
+                } else {
+                    System.out.println("Test3");
+                    account.login();
+                    errorLabel.setText("");
+                    MainFrameController mc = (MainFrameController) Scenes.MAIN.
+                            getController();
+                    mc.fillTabPane();
+                    Scenes.MAIN.setToScene();
+                }
+                usernameField.setText("");
+                passwordField.setText("");
+
+                loginButton.setDisable(false);
+            });
+        }
     }
 
     private void displayErrorAlert() {
